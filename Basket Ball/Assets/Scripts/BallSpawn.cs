fe87f199be32915f19d416667e3f4fde2fabe899
KarlBittman <30643601+KarlBittman@users.noreply.github.com>
2018-03-24 17:05:39 -0700

    BallSpawn

---
+++ Basket Ball/Assets/Scripts/BallSpawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawn : MonoBehaviour {
+
+	Timer spawnTimer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
